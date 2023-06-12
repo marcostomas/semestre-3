d759d803a0d026891d76a29d0e3804c36fe5b218
Marcos <mtomasferreira@hotmail.com>
2023-06-12 17:16:42 -0300

    testes ep2

--- AED II/EP2/run.c
+++ AED II/EP2/run.c
@@ -2,7 +2,7 @@
 int main(int argc, char const *argv[])
 {
 
-	if(argc==1)
+	if (argc == 1)
 	{
 		printf("Please enter one of the options below:\n");
 		printf("Commands:\n");
@@ -13,56 +13,58 @@ int main(int argc, char const *argv[])
 		printf("Exiting now\n");
 		exit(0);
 	}
-	
-	int len,i;
-    printf("How many records do you want to build from dataset?");
-	scanf("%d",&len);
 
-    recordNode *records = getData("data/dataset.csv", len);
-    bTree* tree = createTree("tree.dat",false);
-    
-    for(i=0;i<len;i++)
-    {
-    	insert(tree,&records[i]);
-    }
+	int len, i;
+	printf("How many records do you want to build from dataset?");
+	scanf("%d", &len);
 
-    if(!strcmp(argv[1],"-d"))
-    {
-	    int key;
-	    sscanf(argv[2],"%d",&key);
-	    bool res = removeFromTree(tree,key);
-    	if(res)
-    	{
-    		printf("Successfull Deletion.\n");
-    	}
-    	else
-    	{
-    		printf("Deletion not successful.\n");
-    	}
-    }
+	recordNode *records = getData("data/dataset.csv", len);
+	bTree *tree = createTree("tree.dat", false);
 
-        traverse(tree, tree->root);
+	for (i = 0; i < len; i++)
+	{
+		insert(tree, &records[i]);
+	}
 
-	if(!strcmp(argv[1],"-s"))
-    {
-	    // printf("Time Taken to build tree: %f seconds\n",timeToBuild);
-	    int key;
-	    sscanf(argv[2],"%d",&key);
-	    recordNode* res = search(tree,key);
+	if (!strcmp(argv[1], "-d"))
+	{
+		int key;
+		sscanf(argv[2], "%d", &key);
+		bool res = removeFromTree(tree, key);
+		if (res)
+		{
+			printf("Successfull Deletion.\n");
+		}
+		else
+		{
+			printf("Deletion not successful.\n");
+		}
+	}
 
-		if(res != NULL) {
+	traverse(tree, tree->root);
+
+	if (!strcmp(argv[1], "-s"))
+	{
+		// printf("Time Taken to build tree: %f seconds\n",timeToBuild);
+		int key;
+		sscanf(argv[2], "%d", &key);
+		recordNode *res = search(tree, key);
+
+		if (res != NULL)
+		{
 			printf("key\tcountry\tgrate\tscore\trate\n");
-			printf("%d\t",res->key );
-			printf("%s\t",res->country );
-			printf("%s\t",res->grate );
-			printf("%d\t",res->score );
-			printf("%d\n",res->rate );
+			printf("%d\t", res->key);
+			printf("%s\t", res->country);
+			printf("%s\t", res->grate);
+			printf("%d\t", res->score);
+			printf("%d\n", res->rate);
 
 			free(res);
-		} else
+		}
+		else
 			printf("Record not found!");
-    }
+	}
 
 	free(records);
 	free(tree);
-}	
+}
